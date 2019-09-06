8710eb5e03af3ac09b843e89d2990cf735af9cc5
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2019-09-06 11:17:15 +0000

    bump to php 7

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.0/apache2/php.ini
 
 if [ "${DISABLE_MYSQL}" != "yes" ]; then
 
